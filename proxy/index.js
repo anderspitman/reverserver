db1983ef0c82aef33ae4c43aee4437351e3b58a8
Anders Pitman <tapitman11@gmail.com>
2018-08-23 15:53:39 -0600

    Successfully reading BAM header over proxy

--- proxy/index.js
+++ proxy/index.js
@@ -8,8 +8,18 @@ class ReverserverClient {
       console.log("New ws connection");
       //if (this._ws === undefined) {
       ws.on('message', (message) => {
-        //this.onMessage(JSON.parse(message));
-        this.onMessage(message);
+        if (typeof message === 'string') {
+          try {
+            const parsed = JSON.parse(message);
+            this.onCommand(parsed);
+          }
+          catch(e) {
+            console.log(e);
+          }
+        }
+        else {
+          this.onMessage(message);
+        }
       });
 
       this._ws = ws;
@@ -17,38 +27,89 @@ class ReverserverClient {
     });
 
     this.state = 'idle';
-    this._resolve = null;
+    //this._resolve = null;
+    this._onData = null;
+    this._onEnd = null;
   }
 
   get(url, options) {
-    this.state = 'waiting';
+    this.state = 'receiving';
     this.send({
       type: 'GET',
       url,
       range: options.range,
     });
 
-    return new Promise((resolve, reject) => {
-      this._resolve = resolve;
-    });
+    return {
+      onData: (callback) => {
+        this._onData = callback;
+      },
+      onEnd: (callback) => {
+        this._onEnd = callback;
+      },
+    };
+
+    //return new Promise((resolve, reject) => {
+    //  this._resolve = resolve;
+    //});
   }
 
   send(message) {
     this._ws.send(JSON.stringify(message));
   }
 
+  onCommand(command) {
+    switch(command.type) {
+      case 'start-stream':
+        this.state = 'streaming';
+        break;
+      case 'end-stream':
+        if (this.state === 'streaming') {
+          this.state = 'idle';
+          this._onEnd();
+          this._onData = null;
+          this._onEnd = null;
+        }
+        else {
+          throw "Unexpected end-stream";
+        }
+        break;
+      default:
+        throw "Invalid command type : " + command.type;
+        break;
+    }
+  }
+
   onMessage(message) {
-    console.log(message);
+    //console.log(message);
     switch(this.state) {
       case 'idle':
         break;
-      case 'waiting':
-        this._resolve(message);
-        this._resolve = null;
-        this.state = 'idle';
+      case 'streaming':
+        this._onData(message);
+        break;
+      case 'receiving':
+        this._onData(message);
+        this._onEnd();
+        this._onData = null;
+        this._onEnd = null;
         break;
       default:
         throw "Invalid state: " + this.state;
+        break;
+    }
+  }
+
+  handleClose() {
+    switch(this.state) {
+      case 'streaming':
+        this.send({
+          type: 'command',
+          command: 'interrupt-stream',
+        });
+        break;
+      default:
+        throw "handleClose invalid state: " + this.state;
         break;
     }
   }
@@ -59,6 +120,7 @@ const wss = new WebSocket.Server({ port: 8081 });
 const rsClient = new ReverserverClient();
 
 http.createServer(function(req, res){
+  console.log(req.method);
   if (req.method === 'GET') {
 
     const options = {};
@@ -72,11 +134,22 @@ http.createServer(function(req, res){
       };
     }
 
-    rsClient.get(req.url, options).then((data) => {
-      res.writeHead(200, {'Content-type':'application/octet-stream'});
+    const get = rsClient.get(req.url, options);
+    
+    res.writeHead(200, {'Content-type':'application/octet-stream'});
+
+    get.onData((data) => {
+      console.log("write data");
       res.write(data);
+    });
+
+    get.onEnd(() => {
       res.end();
-    })
+    });
+
+    res.on('close', (e) => {
+      rsClient.handleClose();
+    });
   }
   else {
     res.writeHead(405, {'Content-type':'text/plain'});
--- server/src/index.js
+++ server/src/index.js
@@ -13,27 +13,103 @@ class ReverserverServer {
 
     this._ws = ws;
     this._files = {};
+    this.chunkSize = 1000000;
+    this._interrupt = false;
   }
 
   onMessage(message) {
-    console.log(message);
-    if (message.type === 'GET') {
 
-      if (this._files[message.url] !== undefined) {
+    console.log(message);
+    switch(message.type) {
+      case 'command':
+        switch(message.command) {
+          case 'interrupt-stream':
+            console.log("stream interrupted");
+            this._interrupt = true;
+            break;
+          default:
+            throw "Invalid command: " + message.command;
+            break;
+        }
+        break;
+      case 'GET':
+        if (message.type === 'GET') {
+          if (this._files[message.url] !== undefined) {
+            //reader.readAsText(this._files[message.url]);
+            let file = this._files[message.url];
+
+            if (message.range) {
+              file = file.slice(message.range.start, message.range.end);
+            }
+            console.log(`read file: ${message.url}`);
+
+            console.log(file.size);
+
+            if (file.size <= this.chunkSize) {
+              const reader = new FileReader();
+              reader.onload = (e) => {
+                console.log("done reading");
+                const contents = e.target.result;
+                this.send(contents);
+              };
+              reader.readAsArrayBuffer(file);
+            }
+            else {
+              this.sendChunkedFile(file);
+            }
+          }
+          else {
+            console.log(`File ${message.url} not found`);
+          }
+        }
+        break;
+      default:
+        throw "Invalid message type: " + message.type;
+        break;
+    }
+  }
+
+  sendChunkedFile(file) {
+    const size = file.size;
+
+    this.sendCommand({ type: 'start-stream' });
+
+    const sendChunks = (sendIndex) => {
+
+      if (this._interrupt === true) {
+        this._interrupt = false;
+        return;
+      }
+
+      //console.log(sendIndex);
+
+      if (sendIndex < size) {
+
+        const chunkStart = sendIndex;
+        const chunkEnd = sendIndex + this.chunkSize;
+        const slice = file.slice(chunkStart, chunkEnd);
+
+        console.log(slice.size);
+
         const reader = new FileReader();
         reader.onload = (e) => {
+          //console.log("done reading");
           const contents = e.target.result;
           this.send(contents);
+          sendChunks(sendIndex + slice.size);
         };
-        //reader.readAsText(this._files[message.url]);
-        let file = this._files[message.url];
-
-        if (message.range) {
-          file = file.slice(message.range.start, message.range.end);
-        }
-        reader.readAsArrayBuffer(file);
+        reader.readAsArrayBuffer(slice);
       }
-    }
+      else {
+        this.sendCommand({ type: 'end-stream' });
+      }
+    };
+
+    sendChunks(0);
+  }
+
+  sendCommand(command) {
+    this.send(JSON.stringify(command));
   }
 
   send(message) {
